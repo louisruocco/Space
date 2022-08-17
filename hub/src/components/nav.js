acf4130d17dc2f8cefe1cac0df5c3b2b3e8270c2
Louis Ruocco <louisruocco1@gmail.com>
2022-08-17 16:28:14 +0100

    created basic nav bar

---
+++ hub/src/components/nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Nav(){
+    return (
+        <div className="nav">
+            <nav>
+                <ul>
+                    <li><a href="">Journal</a></li>
+                    <li><a href="">Login/Signup</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Nav;

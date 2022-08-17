2796628138a008b14bfd6cfd09ce2ee976023b63
Louis Ruocco <louisruocco1@gmail.com>
2022-08-17 16:00:26 +0100

    basic server config complete

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const dotenv = require("dotenv");
+const flash = require("connect-flash");
+const app = express();
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server Listening at ${port}`));
